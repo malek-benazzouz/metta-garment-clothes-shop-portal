1ce18f3df22a5809000162528c7ef1b1f559252b
Malek Benazzouz <malek.benazzouz@amadeus.com>
2021-08-23 22:21:46 +0200

    Connect app to firebase 'metta-backend' project, and create a service to connect to firestore db

---
+++ src/app/newsletter-form/newsletter.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface NewsletterSubscription {
+  email: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class RsvpService {
+
+  constructor(private store: AngularFirestore, private auth: AngularFireAuth) {}
+
+  public signIn(email: string, password: string): Promise<any> {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  public signOut(): Promise<any> {
+    return this.auth.signOut();
+  }
+
+  public isAuthenticated(): Observable<boolean> {
+    return this.auth.user.pipe(map(user => user !== null));
+  }
+
+  public getUserEmail(): Observable<string | null | undefined> {
+    return this.auth.user.pipe(map(user => user?.email));
+  }
+
+  public addNewsletterSubscription(subscription: NewsletterSubscription): Promise<DocumentReference<NewsletterSubscription>> {
+    return this.store.collection<NewsletterSubscription>('newsletter-subscriptions').add(subscription);
+  }
+
+  public getAllNewsletterSubscriptions(): Observable<NewsletterSubscription[]> {
+    return this.store.collection<NewsletterSubscription>('newsletter-subscriptions').valueChanges();
+  }
+
+}
