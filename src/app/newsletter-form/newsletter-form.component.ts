9f30044e26dedc22df4f61507290a91667a9beb7
Malek BENAZZOUZ <benazzouzmalek@gmail.com>
2021-08-23 23:27:53 +0200

    On newsletter form submit, if the form is valid, send form input to firestore database and display a feedback toast

--- src/app/newsletter-form/newsletter-form.component.ts
+++ src/app/newsletter-form/newsletter-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { NewsletterSubscriptionService } from './newsletter.service';
 
 @Component({
   selector: 'app-newsletter-form',
@@ -8,12 +9,35 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class NewsletterFormComponent implements OnInit {
 
-  // TODO on form submit send the email address to a new firebase DB
-
   emailControl = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor() {}
+  feedbackMessages: { message: string, isSuccess: boolean }[] = [];
+
+  constructor(private newsletterSubscriptionService: NewsletterSubscriptionService) {}
 
   ngOnInit(): void {}
+
+  submitForm(): void {
+    if (this.emailControl.dirty && this.emailControl.valid) {
+      this.newsletterSubscriptionService.addNewsletterSubscription({
+        email: this.emailControl.value,
+        subscriptionDate: { timestamp: Date.now(), formatted: new Date().toString() }
+      }).then(
+        () => { // Success
+          this.feedbackMessages.push({
+            message: `Thank you for subscribing. We will send our latest updates to ${this.emailControl.value}.`,
+            isSuccess: true
+          });
+        },
+        (error) => { // Error
+          console.error('An error occurred while trying to add a newsletter subscription: ', error);
+          this.feedbackMessages.push({
+            message: 'An error occurred while trying to submit you email. Please try again.',
+            isSuccess: false
+          })
+        }
+      );
+    }
+  }
 
 }
