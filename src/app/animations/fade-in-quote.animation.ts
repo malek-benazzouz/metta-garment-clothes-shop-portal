edbcda162c2f5a03598b7b400cc9104a8815ac9a
Malek BENAZZOUZ <benazzouzmalek@gmail.com>
2022-05-12 23:01:49 +0200

    Add animation: progressively fade in averroes quote while leaf is dropping

---
+++ src/app/animations/fade-in-quote.animation.ts
@@ -0,0 +1,16 @@
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
+
+export const fadeInQuoteAnimation = trigger('fadeInQuote', [
+  state('hidden', style({ opacity: 0 })),
+  state('shown', style({ opacity: 1 })),
+  transition('hidden => shown', [
+    animate(
+      '15s 0s',
+      keyframes([
+        style({ offset: 0.00, opacity: 0 }),
+        style({ offset: 0.75, opacity: 0.5 }),
+        style({ offset: 1.00, opacity: 1 })
+      ])
+    )
+  ])
+]);
