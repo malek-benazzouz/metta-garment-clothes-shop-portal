f7df22abf8763a6b5c88ff33921aa012f0fb598c
Malek Benazzouz <malek.benazzouz@amadeus.com>
2023-02-05 19:08:55 +0100

    Show a page overlay when opening the navbar menu

--- src/app/navbar-menu/navbar-menu.component.ts
+++ src/app/navbar-menu/navbar-menu.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
+import { pageOverlayAnimation } from '../animations/page-overlay.animation';
 
 @Component({
   selector: 'app-navbar-menu',
   templateUrl: './navbar-menu.component.html',
-  styleUrls: ['./navbar-menu.component.scss']
+  styleUrls: ['./navbar-menu.component.scss'],
+  animations: [pageOverlayAnimation]
 })
 export class NavbarMenuComponent implements OnInit {
+
+  showPageOverlay = false;
 
   constructor() {}
 
@@ -15,6 +19,7 @@ export class NavbarMenuComponent implements OnInit {
   // Open the menu on mouse enter
   onMenuEnter(dropdown: NgbDropdown, event: MouseEvent, dropdownId: string): void {
     dropdown.open();
+    this.showPageOverlay = true;
   }
 
   // Close the menu on mouse leave
@@ -22,6 +27,7 @@ export class NavbarMenuComponent implements OnInit {
     // Do not close the menu if the mouse goes into the dropdown menu
     if ((event as any)?.relatedTarget?.parentElement?.id !== dropdownId) {
       dropdown.close();
+      this.showPageOverlay = false;
     }
   }
 
