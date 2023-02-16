5053cd1827edccfad5934a50baaa1a1c27fcbf99
Malek Benazzouz <malek.benazzouz@amadeus.com>
2023-02-16 10:34:23 +0100

    Add products images and start updating the products data

--- src/data/product.model.ts
+++ src/data/product.model.ts
@@ -1,9 +1,10 @@
 export interface Product {
-  id: string; // e.g. '001' or '001prime'
-  name: string; // e.g. '001 KIRGHIZISTAN T'
-  description: string; // e.g. 'Single Craft'
-  price: number; // in EUR
-  imageUrlMobile: string; // e.g. 'assets/products/001-kirghizistan.svg'
-  imageUrlDesktop: string; // e.g. 'assets/products/001-kirghizistan.svg'
-  isAvailable: boolean; // if the product is not available, its leaf will be a normal non-clickable leaf with no number
+  id: string;               // e.g. '001' or '001prime'
+  name: string;             // e.g. '001 METTÄ LIFE SHIRT'
+  description: string;      // e.g. 'The Life Garment'
+  price?: number;           // Optional - in EUR
+  productPageLink: string;  // e.g. 'https://door.metta-garment.com/product/metta-life-shirt/'
+  imageUrlDesktop: string;  // e.g. 'assets/products/001-life-garment-metta-life-shirt-desktop.webp'
+  imageUrlMobile: string;   // e.g. 'assets/products/001-life-garment-metta-life-shirt-mobile.webp'
+  isAvailable: boolean;     // if the product is not available, its leaf will be a normal non-clickable leaf with no number
 }
