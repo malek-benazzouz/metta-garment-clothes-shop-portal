e1cbf968b09364765a77ea59442ddbfaf4f496a5
Malek Benazzouz <malek.benazzouz@amadeus.com>
2023-06-30 18:55:15 +0200

    Update products and images

--- src/data/product.model.ts
+++ src/data/product.model.ts
@@ -10,7 +10,7 @@ export interface Product {
   description: string;              // e.g. 'The Life Garment'
   price?: number;                   // Optional - in EUR
   productPageLink: string;          // e.g. 'https://door.metta-garment.com/product/metta-life-shirt/'
-  imageUrlDesktop: string;          // e.g. 'assets/products/001-life-garment-metta-life-shirt-desktop.webp'
+  imageUrlDesktop: string;          // e.g. 'assets/products/001-life-garment-metta-life-shirt.webp'
   imageUrlMobile: string;           // e.g. 'assets/products/001-life-garment-metta-life-shirt-mobile.webp'
   isAvailable: boolean;             // if the product is not available, its leaf will be a normal non-clickable leaf with no number
 }
